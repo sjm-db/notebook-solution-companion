ba70fa9022e8eb420e606a5cff786f5ca76d29d8
Emanuele Rinaldi <132584476+erinaldidb@users.noreply.github.com>
2024-11-18 12:38:28 -0500

    required databricks-sdk-0.36

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='solacc',
     version='0.1',
     packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=["databricks-sdk==0.36.0"],
     license_files = ('LICENSE',)
 )
